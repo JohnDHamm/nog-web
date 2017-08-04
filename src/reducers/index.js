8c7a96a69fbff9e414052d6a8e237cc647b28378
John Hamm <johndhamm1@gmail.com>
2017-08-03 21:09:49 -0500

    added bootstrap + material-ui, some navbar styling

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,12 +4,15 @@ import UserReducer from './reducer_user';
 import UserPatternsReducer from './reducer_user_patterns';
 import NogTypes from './reducer_nog_types';
 import CurrentPattern from './reducer_current_pattern';
+import Values from './reducer_values';
+
 
 const rootReducer = combineReducers({
 	user: UserReducer,
 	userPatterns: UserPatternsReducer,
 	nogTypes: NogTypes,
-	currentPattern: CurrentPattern
+	currentPattern: CurrentPattern,
+	values: Values
 });
 
 export default rootReducer;
---
+++ src/reducers/reducer_values.js
@@ -0,0 +1,10 @@
+export default function() {
+
+	return {
+		nogRed: '#701626',
+		nogGreen: '#195816',
+		nogBackground: '#222',
+		nogGrayText: '#DDD'
+	}
+
+}
