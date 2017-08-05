29a0d483a0addd2940ab7b1566f4657863a9a1ce
John Hamm <johndhamm1@gmail.com>
2017-08-05 15:55:37 -0500

    snowflake instance test current instance layout

--- src/components/instance_snowflake.js
+++ src/components/instance_snowflake.js
@@ -15,23 +15,37 @@ class InstanceSnowflake extends Component {
 		const styles = {
 			root: {
 				position: 'absolute',
-				top: '50%',
-				left: '50%',
-				backgroundColor: '#333'
+				top: 100,
+				left: 'calc(50% - 210px)',
+				// backgroundColor: '#000'
+			},
+			container: {
+				position: 'relative',
+				width: 420,
+				height: 420,
+				backgroundImage: 'url(../src/img/snowflake_instance_v01.png',
+				backgroundSize: '100% 100%'
 			},
 			light: {
 				width: 20,
 				height: 20,
 				borderRadius: '50%',
 				border: '1px solid #333',
+				position: 'absolute'
 			},
 			lightColor_1: {
+				top: 7,
+				left: 200,
 				backgroundColor: this.getColor(`${instance.lightsColor[0]}`)
 			},
 			lightColor_2: {
+				bottom: 5,
+				right: 5,
 				backgroundColor: this.getColor(`${instance.lightsColor[1]}`)
 			},
 			lightColor_3: {
+				bottom: 5,
+				left: 5,
 				backgroundColor: this.getColor(`${instance.lightsColor[2]}`)
 			},
 		};
@@ -39,11 +53,10 @@ class InstanceSnowflake extends Component {
 
 		return (
 			<div style={styles.root}>
-				<div style={{ ...styles.light, ...styles.lightColor_1 }}>
-				</div>
-				<div style={{ ...styles.light, ...styles.lightColor_2 }}>
-				</div>
-				<div style={{ ...styles.light, ...styles.lightColor_3 }}>
+				<div style={styles.container}>
+					<div style={{ ...styles.light, ...styles.lightColor_1 }} />
+					<div style={{ ...styles.light, ...styles.lightColor_2 }} />
+					<div style={{ ...styles.light, ...styles.lightColor_3 }} />
 				</div>
 			</div>
 		);
