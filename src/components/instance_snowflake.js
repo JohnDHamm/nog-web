41631ea59af5578414bd1ed3982a99f355410577
John Hamm <johndhamm1@gmail.com>
2017-08-07 17:23:26 -0500

    revised bgImg for non-current instances

--- src/components/instance_snowflake.js
+++ src/components/instance_snowflake.js
@@ -27,7 +27,7 @@ class InstanceSnowflake extends Component {
 				position: 'relative',
 				width: containerSize,
 				height: containerSize,
-				backgroundImage: 'url(../src/img/snowflake_currentInstance_bgImage.png',
+				backgroundImage: 'url(../src/img/snowflake_otherInstance_bgImage.png',
 				backgroundSize: '100% 100%'
 			},
 			labelDiv: {
