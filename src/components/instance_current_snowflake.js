238a4ff4c9fecbbaf94adb22e7d9d67fc4deda66
John Hamm <johndhamm1@gmail.com>
2017-09-15 16:09:13 -0500

    moved snowflakeLightLocations to helper function

--- src/components/instance_current_snowflake.js
+++ src/components/instance_current_snowflake.js
@@ -5,6 +5,8 @@ import { connect } from 'react-redux';
 import { updateLight } from '../actions';
 import values from '../styles/values';
 
+import { snowflakeLightLocations } from '../helpers/snowflakeLightLocations';
+
 class InstanceCurrentSnowflake extends Component {
 
 	getColor(colNum) {
@@ -41,128 +43,7 @@ class InstanceCurrentSnowflake extends Component {
 			lightDia = containerSize * (20 / 420),
 			lightOffset = lightDia / 2,
 			lightBorderSize = containerSize < 210 ? 0 : 1;
-		const lightLocations = [
-			{ lightNum: 0,
-				top: '36%',
-				left: `calc(50% - ${lightOffset}px)`,
-			},
-			{ lightNum: 1,
-				top: '13%',
-				left: '38.2%'
-			},
-			{ lightNum: 2,
-				top: '1.7%',
-				left: `calc(50% - ${lightOffset}px)`,
-			},
-			{ lightNum: 3,
-				top: '13%',
-				right: '38%'
-			},
-			{ lightNum: 4,
-				top: '25.3%',
-				right: '34.6%'
-			},
-			{ lightNum: 5,
-				top: '41.7%',
-				right: '37.4%'
-			},
-			{ lightNum: 6,
-				top: '22.1%',
-				right: '22.6%'
-			},
-			{ lightNum: 7,
-				top: '24.55%',
-				right: '7.65%'
-			},
-			{ lightNum: 8,
-				top: '38.6%',
-				right: '13%'
-			},
-			{ lightNum: 9,
-				top: `calc(50% - ${lightOffset}px)`,
-				right: '22.05%'
-			},
-			{ lightNum: 10,
-				bottom: '41.7%',
-				right: '37.4%'
-			},
-			{ lightNum: 11,
-				bottom: '38.6%',
-				right: '13%'
-			},
-			{ lightNum: 12,
-				bottom: '24.62%',
-				right: '7.65%'
-			},
-			{ lightNum: 13,
-				bottom: '22.1%',
-				right: '22.6%'
-			},
-			{ lightNum: 14,
-				bottom: '25.3%',
-				right: '34.6%'
-			},
-			{ lightNum: 15,
-				bottom: '36%',
-				left: `calc(50% - ${lightOffset}px)`
-			},
-			{ lightNum: 16,
-				bottom: '13%',
-				right: '38%'
-			},
-			{ lightNum: 17,
-				bottom: '1.7%',
-				left: `calc(50% - ${lightOffset}px)`
-			},
-			{ lightNum: 18,
-				bottom: '13%',
-				left: '38%'
-			},
-			{ lightNum: 19,
-				bottom: '25.3%',
-				left: '34.8%'
-			},
-			{ lightNum: 20,
-				bottom: '41.7%',
-				left: '37.4%'
-			},
-			{ lightNum: 21,
-				bottom: '22.1%',
-				left: '22.6%'
-			},
-			{ lightNum: 22,
-				bottom: '24.62%',
-				left: '7.65%'
-			},
-			{ lightNum: 23,
-				bottom: '38.6%',
-				left: '13%'
-			},
-			{ lightNum: 24,
-				bottom: `calc(50% - ${lightOffset}px)`,
-				left: '22.05%'
-			},
-			{ lightNum: 25,
-				top: '41.7%',
-				left: '37.4%'
-			},
-			{ lightNum: 26,
-				top: '38.6%',
-				left: '13%'
-			},
-			{ lightNum: 27,
-				top: '24.62%',
-				left: '7.65%'
-			},
-			{ lightNum: 28,
-				top: '22.1%',
-				left: '22.6%'
-			},
-			{ lightNum: 29,
-				top: '25.3%',
-				left: '34.8%'
-			}
-		]
+		const lightLocations = snowflakeLightLocations(lightOffset);
 
 		const styles = {
 			lightCircle: {
@@ -211,7 +92,6 @@ class InstanceCurrentSnowflake extends Component {
 			labelDiv: {
 				position: 'absolute',
 				width: instanceSize / 8,
-				// border: '1px dotted green',
 				top: `calc(50% - ${instanceSize / 15}px)`,
 				left: `calc(50% - ${instanceSize / 16}px)`,
 			},
---
+++ src/helpers/snowflakeLightLocations.js
@@ -0,0 +1,124 @@
+export function snowflakeLightLocations(lightOffset) {
+ return [
+		{ lightNum: 0,
+			top: '36%',
+			left: `calc(50% - ${lightOffset}px)`,
+		},
+		{ lightNum: 1,
+			top: '13%',
+			left: '38.2%'
+		},
+		{ lightNum: 2,
+			top: '1.7%',
+			left: `calc(50% - ${lightOffset}px)`,
+		},
+		{ lightNum: 3,
+			top: '13%',
+			right: '38%'
+		},
+		{ lightNum: 4,
+			top: '25.3%',
+			right: '34.6%'
+		},
+		{ lightNum: 5,
+			top: '41.7%',
+			right: '37.4%'
+		},
+		{ lightNum: 6,
+			top: '22.1%',
+			right: '22.6%'
+		},
+		{ lightNum: 7,
+			top: '24.55%',
+			right: '7.65%'
+		},
+		{ lightNum: 8,
+			top: '38.6%',
+			right: '13%'
+		},
+		{ lightNum: 9,
+			top: `calc(50% - ${lightOffset}px)`,
+			right: '22.05%'
+		},
+		{ lightNum: 10,
+			bottom: '41.7%',
+			right: '37.4%'
+		},
+		{ lightNum: 11,
+			bottom: '38.6%',
+			right: '13%'
+		},
+		{ lightNum: 12,
+			bottom: '24.62%',
+			right: '7.65%'
+		},
+		{ lightNum: 13,
+			bottom: '22.1%',
+			right: '22.6%'
+		},
+		{ lightNum: 14,
+			bottom: '25.3%',
+			right: '34.6%'
+		},
+		{ lightNum: 15,
+			bottom: '36%',
+			left: `calc(50% - ${lightOffset}px)`
+		},
+		{ lightNum: 16,
+			bottom: '13%',
+			right: '38%'
+		},
+		{ lightNum: 17,
+			bottom: '1.7%',
+			left: `calc(50% - ${lightOffset}px)`
+		},
+		{ lightNum: 18,
+			bottom: '13%',
+			left: '38%'
+		},
+		{ lightNum: 19,
+			bottom: '25.3%',
+			left: '34.8%'
+		},
+		{ lightNum: 20,
+			bottom: '41.7%',
+			left: '37.4%'
+		},
+		{ lightNum: 21,
+			bottom: '22.1%',
+			left: '22.6%'
+		},
+		{ lightNum: 22,
+			bottom: '24.62%',
+			left: '7.65%'
+		},
+		{ lightNum: 23,
+			bottom: '38.6%',
+			left: '13%'
+		},
+		{ lightNum: 24,
+			bottom: `calc(50% - ${lightOffset}px)`,
+			left: '22.05%'
+		},
+		{ lightNum: 25,
+			top: '41.7%',
+			left: '37.4%'
+		},
+		{ lightNum: 26,
+			top: '38.6%',
+			left: '13%'
+		},
+		{ lightNum: 27,
+			top: '24.62%',
+			left: '7.65%'
+		},
+		{ lightNum: 28,
+			top: '22.1%',
+			left: '22.6%'
+		},
+		{ lightNum: 29,
+			top: '25.3%',
+			left: '34.8%'
+		}
+	]
+}
