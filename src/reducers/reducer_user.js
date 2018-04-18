b45d0460388c37f9f102c8dd50076e0a09146611
John Hamm <johndhamm1@gmail.com>
2018-04-18 08:58:17 -0500

    save for demo

--- src/reducers/reducer_user.js
+++ src/reducers/reducer_user.js
@@ -3,14 +3,17 @@ export default function() {
 	return {
 		user:
 			{
-				// _id: '59824b41172824a3e8d0d5d6', //heroku
-				// name: 'Buddy Elf',
-				// email: 'testUser@nog.com',
-				// iconUrl: '../src/img/buddy_elf.png'
-				_id: '598296cd172824a3e8d0d5e4', //heroku
-				name: 'Santa',
-				email: 'santa@nog.com',
-				iconUrl: '../src/img/Santa_profile.jpg'
+				_id: '59824b41172824a3e8d0d5d6', //heroku
+				name: 'Buddy Elf',
+				email: 'testUser@nog.com',
+				iconUrl: '../src/img/buddy_elf.png'
+				// _id: '598296cd172824a3e8d0d5e4', //heroku
+				// name: 'Santa',
+				// email: 'santa@nog.com',
+				// iconUrl: '../src/img/Santa_profile.jpg'
+
+				// _id: '598246abbee2c891bd2cedc8', //local dev
+				// name: 'SantaDev',
 			}
 	}
 }
@@ -18,5 +21,3 @@ export default function() {
 
 
 
-				// _id: '598246abbee2c891bd2cedc8', //local dev
-				// name: 'SantaDev',
